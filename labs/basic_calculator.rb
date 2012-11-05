e8cba06cea42c6588aa1f4d0fc85d7c3d9adf5d0
Jasmine Chabra <jchabra@gmail.com>
2012-11-05 12:06:34 -0500

    basic calc 1 and 2

---
+++ labs/basic_calculator.rb
@@ -0,0 +1,22 @@
+#require 'pry'
+
+puts "Do you want to (a)dd, (s)ubtract or (q)uit?"
+response = gets.chomp
+
+while response != 'q'
+	puts "Enter First number"
+	first = gets.to_f #f is for floating number
+	puts "Enter Second number"
+	second = gets.to_f
+
+	if response == 'a'
+		total = first + second
+	elsif response == 's'
+		total = first - second
+	end
+
+	puts "The result of #{first} and #{second} is #{total}"
+	puts "Do you want to (a)dd, (s)ubtract or (q)uit?"
+	response = gets.chomp
+end
+
